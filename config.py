1d829ecf31a72e77713d45d83f5d512b555ccfa5
Arya Sardesai <122335171+Arya230503@users.noreply.github.com>
2024-05-22 21:19:33 -0700

    Uploaded local files of Project1

---
+++ config.py
@@ -0,0 +1,4 @@
+# spotipy wraps the official spotify api providing simple python functions.
+# TODO: Replace these two variables with the client_id and client_secret that you generated
+CLIENT_ID = "3e55c34955d84067902262ed25b37a76"
+CLIENT_SECRET = "3ca7959cc8a0487cace3b031c0a756c4"
